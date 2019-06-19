0bff7b17a78268c9fe3c26f8cc94438145034ac6
Reshetnyak <dreshetnyak@amt.ru>
2019-06-19 18:21:57 +0300

    added dir list

--- parser1.py
+++ parser1.py
@@ -2,16 +2,17 @@ import textfsm
 import os
 import re
 import xlwt
-
+import tkinter as tk
+from tkinter import filedialog
 def fnParse(fname, re_table, outfile, i):
     input_file = open(fname, encoding='utf-8')
     raw_text_data = input_file.read()
     input_file.close()
-    
+
     fsm_results = re_table.ParseText(raw_text_data)
-    
-    
-    
+
+
+
     # ...now all row's which were parsed by TextFSM
     counter = 0
     for row in fsm_results:
@@ -22,12 +23,12 @@ def fnParse(fname, re_table, outfile, i):
         counter += 1
     print("Write %d records" % counter)
 
-
+directory=filedialog.askdirectory()#Выбор директории по диалоговому окну
 # Читаем файл шаблона:
-# Run the text through the FSM. 
-# The argument 'template' is a file handle and 'raw_text_data' is a 
+# Run the text through the FSM.
+# The argument 'template' is a file handle and 'raw_text_data' is a
 # string with the content from the show_inventory.txt file
-template = open("c:/config/show_inventory_multiple.textfsm")
+template = open(directory+"/show_inventory_multiple.textfsm")
 re_table = textfsm.TextFSM(template)
 
 # новая книга формата Excel:
@@ -36,11 +37,11 @@ wbk = xlwt.Workbook('utf-8')
 sheet = wbk.add_sheet('sheet 1')
 
 #создаем список файлов, которые будем читать
-directory='c:/config'
+
 files=os.listdir(directory)
 
 # Результат пока что запишем в файл csv:
-outfile_name = open("c:/config/outfile.csv", "w+")
+outfile_name = open(directory+"/outfile.csv", "w+")
 outfile = outfile_name
 
 # Display result as CSV and write it to the output file
@@ -49,7 +50,7 @@ print(re_table.header)
 for s in re_table.header:
     outfile.write("%s;" % s)
 outfile.write("\n")
-    
+
 #открываем файлы поочередно
 trns=str.maketrans('','','\t\n\v\f\r')
 for i,str in enumerate(files):
